569c40fe2c8d31fa823e4b9b07cf2552dd7a9961
Obayed12 <ullahtarik300@gmail.com>
2026-03-01 11:11:14 +0600

    Convert web from submodule to normal folder

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Jost } from "next/font/google";
+import "./globals.css";
+const jost = Jost({
+  subsets: ["latin"],
+  variable: "--font-jost",
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${jost.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
